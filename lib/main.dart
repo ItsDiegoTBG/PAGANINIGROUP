b0cdf11c96199d8e5c52cb79afa68fcf096a2d6d
klashz <117329019+CristhianSantacruz@users.noreply.github.com>
2024-09-27 10:37:02 -0500

    splash screen paganini

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,17 @@ import 'package:paganini/presentation/pages/login_page.dart';
 import 'package:paganini/presentation/pages/qr_pages.dart';
 import 'package:paganini/presentation/pages/scanner_page.dart';
 import 'package:paganini/presentation/pages/wallet_page.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  setup();
   runApp(const MainApp());
+}
+
+void setup() async {
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
 }
 
 class MainApp extends StatelessWidget {
