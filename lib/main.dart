48c36cb7d7200e0e69e665d3fd7e29ee4155b286
klashz <117329019+CristhianSantacruz@users.noreply.github.com>
2025-01-11 18:14:47 -0500

    Nueva animacion de inicio de aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:paganini/app_loader.dart';
 import 'package:paganini/core/routes/app_routes.dart';
 import 'package:paganini/core/theme/app_theme.dart';
 import 'package:paganini/data/datasources/credit_card_datasource.dart';
@@ -14,6 +16,7 @@ import 'package:paganini/data/repositories/credit_card_repository_impl.dart';
 import 'package:paganini/domain/usecases/credit_cards_use_case.dart';
 import 'package:paganini/domain/usecases/contact_use_case.dart';
 import 'package:paganini/firebase_options.dart';
+import 'package:paganini/main_app.dart';
 import 'package:paganini/presentation/pages/screens.dart';
 import 'package:paganini/presentation/providers/contact_provider.dart';
 import 'package:paganini/presentation/providers/credit_card_provider.dart';
@@ -48,6 +51,8 @@ void main() async {
 
   await Future.delayed(const Duration(seconds: 2));
   FlutterNativeSplash.remove();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
   runApp(
     MultiProvider(
       providers: [
@@ -62,7 +67,7 @@ void main() async {
         ChangeNotifierProvider(lazy: false, create: (_) => ThemeProvider()),
         ChangeNotifierProvider(lazy: false,create: (_) => IntroductionProvider()),
       ],
-      child: const MainApp(),
+      child: const AppLoader(),
     ),
   );
 }
@@ -73,69 +78,3 @@ void setup() async {
   WidgetsFlutterBinding.ensureInitialized();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    Provider.of<UserProvider>(context, listen: false).initializeUser();
-    final isIntroductionPage = Provider.of<IntroductionProvider>(context).isIntroductionPage;
-
-    return MaterialApp(
-        title: 'Paganini',
-        debugShowCheckedModeBanner: false,
-        initialRoute: isIntroductionPage ? Routes.APPTUTORIALSCREEN : Routes.INITIAL,
-        routes: {
-          Routes.INITIAL: (context) => const InitialPage(),
-          Routes.HOME: (context) => const HomePage(),
-          Routes.LOGIN: (context) => const LoginPage(),
-          Routes.QRPAGE: (context) => const QrPage(),
-          Routes.WALLETPAGE: (context) => const WalletPage(),
-          Routes.CARDPAGE: (context) => const CardPage(),
-          Routes.CARDDELETEPAGE: (context) => const CardDeletePage(),
-          Routes.REGISTER: (context) => const RegisterPage(),
-          Routes.RECHARGE: (context) => const RechargePage(),
-          Routes.RECEIPTRANSFER: (context) => TransferReceipt(),
-          Routes.TRANSFERPAGE: (context) => const TransferPage(),
-          Routes.INTRODUCTIONPAGE: (context) => const OnBoardingPage(),
-          Routes.APPTUTORIALSCREEN : (context) => const AppTutorialScreen(),
-          Routes.NAVIGATIONPAGE: (context) => const NavigationPage(),
-          Routes.RETURNAMOUNTPAGE: (context) => const ReturnAmountPage(),
-          Routes.FORGETPASSWORD : (context) =>  ForgetPasswordPage(),
-          Routes.SETTINGSPAGE: (context) => const SettingPage(),
-        },
-        theme: Provider.of<ThemeProvider>(context).isDarkMode
-            ? AppTheme().themeDarkMode()
-            : AppTheme().themeLightMode());
-  }
-  /* return DevicePreview(
-      enabled: true,
-      builder: (context) => MaterialApp(
-        // ignore: deprecated_member_use
-        useInheritedMediaQuery: true,
-        locale: DevicePreview.locale(
-            context), // Simula la configuración regional del dispositivo
-        builder: DevicePreview.appBuilder,
-        title: 'Paganini',
-        debugShowCheckedModeBanner: false,
-        initialRoute: Routes.INITIAL,
-        routes: {
-          Routes.INITIAL: (context) => const InitialPage(),
-          Routes.HOME: (context) => const HomePage(),
-          Routes.LOGIN: (context) => const LoginPage(),
-          Routes.QRPAGE: (context) => const QrPage(),
-          Routes.WALLETPAGE: (context) => const WalletPage(),
-          Routes.CARDPAGE: (context) => const CardPage(),
-          Routes.CARDDELETEPAGE: (context) => const CardDeletePage(),
-          Routes.REGISTER: (context) => const RegisterPage(),
-          Routes.RECHARGE: (context) => const RechargePage(),
-          Routes.RECEIPTRANSFER: (context) => TransferReceipt(),
-          Routes.TRANSFERPAGE: (context) => const TransferPage(),
-        },
-        theme: ThemeData(
-            appBarTheme: const AppBarTheme(color: Colors.white),
-            scaffoldBackgroundColor: Colors.white),
-      ),
-    );
-  }*/
-}
---
+++ lib/main_app.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:paganini/app_data.dart';
+import 'package:paganini/core/routes/app_routes.dart';
+import 'package:paganini/core/theme/app_theme.dart';
+import 'package:paganini/presentation/pages/forget_password_page.dart';
+import 'package:paganini/presentation/providers/introduction_provider.dart';
+import 'package:paganini/presentation/providers/theme_provider.dart';
+import 'package:paganini/presentation/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:paganini/presentation/pages/screens.dart';
+
+class MainApp extends StatelessWidget {
+  final AppData appData;
+  const MainApp({super.key, required this.appData});
+  @override
+  Widget build(BuildContext context) {
+    Provider.of<UserProvider>(context, listen: false).initializeUser();
+    final isIntroductionPage = Provider.of<IntroductionProvider>(context).isIntroductionPage;
+
+    return MaterialApp(
+        title: 'Paganini',
+        debugShowCheckedModeBanner: false,
+        initialRoute: isIntroductionPage ? Routes.APPTUTORIALSCREEN : Routes.INITIAL,
+        routes: {
+          Routes.INITIAL: (context) => const InitialPage(),
+          Routes.HOME: (context) => const HomePage(),
+          Routes.LOGIN: (context) => const LoginPage(),
+          Routes.QRPAGE: (context) => const QrPage(),
+          Routes.WALLETPAGE: (context) => const WalletPage(),
+          Routes.CARDPAGE: (context) => const CardPage(),
+          Routes.CARDDELETEPAGE: (context) => const CardDeletePage(),
+          Routes.REGISTER: (context) => const RegisterPage(),
+          Routes.RECHARGE: (context) => const RechargePage(),
+          Routes.RECEIPTRANSFER: (context) => TransferReceipt(),
+          Routes.TRANSFERPAGE: (context) => const TransferPage(),
+          Routes.INTRODUCTIONPAGE: (context) => const OnBoardingPage(),
+          Routes.APPTUTORIALSCREEN : (context) => const AppTutorialScreen(),
+          Routes.NAVIGATIONPAGE: (context) => const NavigationPage(),
+          Routes.RETURNAMOUNTPAGE: (context) => const ReturnAmountPage(),
+          Routes.FORGETPASSWORD : (context) => const   ForgetPasswordPage(),
+          Routes.SETTINGSPAGE: (context) => const SettingPage(),
+        },
+        theme: Provider.of<ThemeProvider>(context).isDarkMode
+            ? AppTheme().themeDarkMode()
+            : AppTheme().themeLightMode());
+  }
+  /* return DevicePreview(
+      enabled: true,
+      builder: (context) => MaterialApp(
+        // ignore: deprecated_member_use
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(
+            context), // Simula la configuración regional del dispositivo
+        builder: DevicePreview.appBuilder,
+        title: 'Paganini',
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.INITIAL,
+        routes: {
+          Routes.INITIAL: (context) => const InitialPage(),
+          Routes.HOME: (context) => const HomePage(),
+          Routes.LOGIN: (context) => const LoginPage(),
+          Routes.QRPAGE: (context) => const QrPage(),
+          Routes.WALLETPAGE: (context) => const WalletPage(),
+          Routes.CARDPAGE: (context) => const CardPage(),
+          Routes.CARDDELETEPAGE: (context) => const CardDeletePage(),
+          Routes.REGISTER: (context) => const RegisterPage(),
+          Routes.RECHARGE: (context) => const RechargePage(),
+          Routes.RECEIPTRANSFER: (context) => TransferReceipt(),
+          Routes.TRANSFERPAGE: (context) => const TransferPage(),
+        },
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(color: Colors.white),
+            scaffoldBackgroundColor: Colors.white),
+      ),
+    );
+  }*/
+}
