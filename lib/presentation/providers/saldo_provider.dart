5db55eb694cc98723f5f60b88493427f8310effa
ItsDiegoTBG <diealcon@espol.edu.ec>
2025-01-12 20:52:12 -0500

    GAGEGIGAGOGAGIGAGO

--- lib/presentation/providers/saldo_provider.dart
+++ lib/presentation/providers/saldo_provider.dart
@@ -1,16 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:paganini/presentation/providers/user_provider.dart';
 
 class SaldoProvider with ChangeNotifier {
-  final DatabaseReference _saldoRef =
-      FirebaseDatabase.instance.ref().child('saldo');
-  
+
+  DatabaseReference? _saldoRef;
   double _saldo = 0.0;
- 
+
   double get saldo => _saldo;
 
-    SaldoProvider() {
-    _saldoRef.onValue.listen((event) {
+  SaldoProvider(String userId) {
+    _initializeSaldo(userId); // Método para inicializar saldoRef y escuchar cambios
+  }
+
+  void _initializeSaldo(String userId) {
+    // Inicializa la referencia del saldo para el usuario específico
+    _saldoRef = FirebaseDatabase.instance.ref().child('users/$userId/saldo');
+    
+    // Escucha cambios en tiempo real
+    _saldoRef?.onValue.listen((event) {
       final newSaldo = event.snapshot.value as double?;
       if (newSaldo != null) {
         _saldo = newSaldo;
@@ -18,7 +27,6 @@ class SaldoProvider with ChangeNotifier {
       }
     });
   }
-
   Future<void> agregar() async {
     _saldo += 1000;
     await _updateSaldoInFirebase();
@@ -36,7 +44,7 @@ class SaldoProvider with ChangeNotifier {
 
   Future<void> _updateSaldoInFirebase() async {
     try {
-      await _saldoRef.set(_saldo);
+      await _saldoRef?.set(_saldo);
     } catch (e) {
       debugPrint("Error updating saldo in Firebase: $e");
     }
